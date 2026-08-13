2e12d48a2855e82dfd43e1fbb038213b63f718f9
tizsalvatore <tizianosalvatore@gmail.com>
2026-08-13 18:51:57 +0200

    Ricerche a capo e interruttore Elenco/Mappa piu' evidente

--- docs/sw.js
+++ docs/sw.js
@@ -11,7 +11,7 @@
    scarichera' la nuova versione invece di riusare la vecchia.
    ========================================================================= */
 
-const VERSIONE = 'interpelli-v9';
+const VERSIONE = 'interpelli-v10';
 const CONTENITORE_APP = `${VERSIONE}-app`;
 const CONTENITORE_DATI = `${VERSIONE}-dati`;
 
